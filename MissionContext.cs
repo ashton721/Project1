a849fde87b8775d4a787f0479dd7dfc88b5b33e7
ashton721 <ashton.murray@gmail.com>
2016-10-26 12:03:59 -0600

    Add files via upload
    THIS IS IN THE DAL FOLDER

---
+++ MissionContext.cs
@@ -0,0 +1,22 @@
+﻿using Project1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project1.DAL
+{
+    public class MissionContext : DbContext
+    {
+        public MissionContext()
+            : base("MissionContext")
+        {
+
+        }
+
+        public DbSet<Missions> Missions { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<MissionQuestions> MissionQuestions { get; set; }
+    }
+}
